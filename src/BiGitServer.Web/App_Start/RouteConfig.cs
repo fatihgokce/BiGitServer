5b747d5d8d997c9b7ee5d4d5a48b4762596c02b2
fatih gokce <fatihgokce07@gmail.com>
2017-05-05 01:14:14 +0300

    first commit

---
+++ src/BiGitServer.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           // dist / index.html
+            routes.MapRoute(
+                name: "StaticHtml",
+                url: "dist/index.html",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/BiGitServer.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Request.Url.Host == "localhost")
+            {
+                return Redirect("/dist/index.html");
+            }
+            return View();
+        }
+        public JsonResult FakeData()
+        {
+            object obj = new{ id = 421, name = "fatih" };
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
